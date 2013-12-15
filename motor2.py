5f6a686c0cf5e84785349adc7549861d494ce681
Lorenzo <lorenzo.milluccioutlook.com>
2013-12-15 12:49:30 +0100

    codice funzionante iniziale

--- motor2.py
+++ motor2.py
@@ -1,33 +1,21 @@
 import RPi.GPIO as io
 io.setmode(io.BCM)
-
-io.cleanup() 
-
+ 
 in1_pin = 4
 in2_pin = 17
 pwm_pin= 23
-in3_pin = 24
-in4_pin = 25
-pwm2_pin = 22
  
 io.setup(in1_pin, io.OUT)
 io.setup(in2_pin, io.OUT)
 io.setup(pwm_pin, io.OUT)
-io.setup(in3_pin, io.OUT)
-io.setup(in4_pin, io.OUT)
-io.setup(pwm2_pin, io.OUT)
-p=io.PWM(pwm_pin, 500)
-q=io.PWM(pwm2_pin, 500)
 
+p=io.PWM(pwm_pin, 500)
 
 p.start(0)
-p.ChangeDutyCycle(100)
-q.start(0)
-q.ChangeDutyCycle(100)
+p.ChangeDutyCycle(50)
 
-def changeSpeed(value1, value2 ):
-	p.ChangeDutyCycle(value1)
-	q.ChangeDutyCycle(value2)
+def changeSpeed(value):
+	p.ChangeDutyCycle(value)
 
 
 #def set(property, value):
@@ -50,37 +38,21 @@ def clockwise():
 def counter_clockwise():
     io.output(in1_pin, False)
     io.output(in2_pin, True)
-
-def orario():
-	io.output(in3_pin,True)
-	io.output(in4_pin,False)
-	
-def antiorario():
-	io.output(in3_pin,False)
-	io.output(in4_pin,True)
-	
+ 
 clockwise()
-orario()
 
  
 try:
 	while True:
 		
 		cmd = raw_input("Command PWM duty cycle")
-		dutyCy1 = int(cmd[0]) * 11
-		dutyCy2 = int(cmd[1]) * 11
-		direction=cmd[2]
-		direction2=cmd[3]
-		changeSpeed(dutyCy1,dutyCy2)
+		dutyCy = int(cmd[0]) * 11
+		direction=cmd[1]
+		changeSpeed(dutyCy)
 		if direction == "f":
 			clockwise()
 		else: 
 			counter_clockwise()
-		
-		if direction2 == "f":
-			orario()
-		else:
-			antiorario()
 
 except KeyboardInterrupt:
 	pass
